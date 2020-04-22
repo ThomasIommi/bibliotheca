4ac4625b1e0518e067621478ca90e33fe8244c38
Thomas Iommi <tiommi@3di.it>
2020-04-22 23:26:07 +0200

    chore: renamed scope

--- projects/iommi/ngxs-ionic-storage/karma.conf.js
+++ projects/iommi/ngxs-ionic-storage/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/tiommi/ngxs-ionic-storage'),
+      dir: require('path').join(__dirname, '../../../coverage/iommi/ngxs-ionic-storage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
